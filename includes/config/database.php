13334080893fbd4e81fc5670eca1600c022e98f4
ncastro@vozy.co <ncastro@vozy.co>
2024-12-21 23:47:44 -0300

    fix bd

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,13 +1,23 @@
 <?php 
 
 function conectarDb() : mysqli {
-    $db = new mysqli ("localhost", "root", "nina1436", "bienesraices_crud");
+    // Obtener las variables de entorno
+    $host = getenv('MYSQL_HOST');  // Dirección del host
+    $username = getenv('MYSQL_USERNAME');  // Nombre de usuario
+    $password = getenv('MYSQL_PASSWORD');  // Contraseña
+    $database = getenv('MYSQL_DATABASE');  // Nombre de la base de datos
+    $port = getenv('MYSQL_PORT');  // Puerto de conexión
 
-   if (!$db) {
-       echo "Error no se pudo conectar";
+    // Crear la conexión a la base de datos
+    $db = new mysqli($host, $username, $password, $database, $port);
+
+    // Verificar si la conexión fue exitosa
+    if ($db->connect_error) {
+        echo "Error: No se pudo conectar a la base de datos. " . $db->connect_error;
         exit;
-   } 
-   return $db;
+    }
+
+    return $db;
 }
 
 ?>
