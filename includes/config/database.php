9ca2bab11c8324ef3cf07e988cb69e355679c66e
ncastro@vozy.co <ncastro@vozy.co>
2024-12-21 23:56:02 -0300

    I will revert the changes."

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,23 +1,13 @@
 <?php 
 
 function conectarDb() : mysqli {
-    // Obtener las variables de entorno
-    $host = getenv('MYSQL_HOST');  // Dirección del host
-    $username = getenv('MYSQL_USERNAME');  // Nombre de usuario
-    $password = getenv('MYSQL_PASSWORD');  // Contraseña
-    $database = getenv('MYSQL_DATABASE');  // Nombre de la base de datos
-    $port = getenv('MYSQL_PORT');  // Puerto de conexión
+    $db = new mysqli ("localhost", "root", "nina1436", "bienesraices_crud");
 
-    // Crear la conexión a la base de datos
-    $db = new mysqli($host, $username, $password, $database, $port);
-
-    // Verificar si la conexión fue exitosa
-    if ($db->connect_error) {
-        echo "Error: No se pudo conectar a la base de datos. " . $db->connect_error;
+   if (!$db) {
+       echo "Error no se pudo conectar";
         exit;
-    }
-
-    return $db;
+   } 
+   return $db;
 }
 
 ?>
